ee09e3089f6c6ca6f067228a0398aafae36af4a4
Anon <anon@example.com>
2025-08-30 15:28:35 +0200

    menyknapp i popup

--- main.js
+++ main.js
@@ -393,7 +393,7 @@ fetch('data/gazarondellen.geojson')
         const latlng = layer.getLatLng();
 		const popup = `
 			<strong>${feature.properties.name || "Gazarondellen"}</strong><br>
-			<strong>Aktivitet:</strong> Se schema i menyn till höger.<br>
+			<strong>Aktivitet:</strong> Se schema ☰ i menyn till höger.<br>
 			<button class="btn route-btn" data-lat="${latlng.lat}" data-lng="${latlng.lng}">Visa rutt</button>
 		`;
         layer.bindPopup(popup);
